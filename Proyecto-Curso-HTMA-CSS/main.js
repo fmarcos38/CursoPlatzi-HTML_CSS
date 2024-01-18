4971dcc7e4e50cbbfcc7c6dde30bca9aa613fa0e
fmarcos38 <fmarcos_23@hotmail.com>
2024-01-18 10:54:10 -0300

    vistasOk

--- Proyecto-Curso-HTMA-CSS/main.js
+++ Proyecto-Curso-HTMA-CSS/main.js
@@ -6,6 +6,9 @@ const menuHamburgesa = document.querySelector('.menu');
 const iconoCarrito = document.querySelector('.navbar-shopping-cart');
 const shopingCartContainer = document.querySelector('#shopingCartContainer');
 const cardContainer = document.querySelector('.cards-container');
+const productDetailContainer = document.querySelector('#productDetail');
+//creo selector para el btn de cerrar el detalle del producto
+const btnCloseProductDetail = document.querySelector('.product-detail-close');
 
 //agrego evento click al menu de email
 menuEmail.addEventListener('click', menuEmailClick);
@@ -13,47 +16,8 @@ menuEmail.addEventListener('click', menuEmailClick);
 menuHamburgesa.addEventListener('click', menuHamburgesaClick);
 //agrego evento click al icono de carrito
 iconoCarrito.addEventListener('click', iconoCarritoClick);
-
-//funcion para mostrar el menu de email
-function menuEmailClick() {
-    const isAsaideClosed = shopingCartContainer.classList.contains('inactive'); //si el menu de carrito esta cerrado
-
-    if(!isAsaideClosed) { //si el menu de carrito esta abierto
-        //cierro el menu de carrito
-        shopingCartContainer.classList.add('inactive');
-    }
-
-    desktopMenu.classList.toggle('inactive'); //el nombre de la clase va sin el punto
-}
-//funcion para mostrar el menu de hamburgesa
-function menuHamburgesaClick() {
-    const isAsaideClosed = shopingCartContainer.classList.contains('inactive'); //si el menu de carrito esta cerrado
-    
-    if(!isAsaideClosed) { //si el menu de carrito esta abierto
-        //cierro el menu de carrito
-        shopingCartContainer.classList.add('inactive');
-    }
-
-    menuMobile.classList.toggle('inactive');
-}
-//funcion para mostrar el menu de carrito
-function iconoCarritoClick() {
-    const isMenuMobileClosed = menuMobile.classList.contains('inactive'); //si el menu de hamburgesa esta cerrado
-    const isMenuEmailClosed = desktopMenu.classList.contains('inactive'); //si el menu de email esta cerrado
-    
-    if(!isMenuMobileClosed) { //si el menu de hamburgesa esta abierto
-        //cierro el menu de hamburgesa
-        menuMobile.classList.add('inactive');
-    }
-
-    if(!isMenuEmailClosed) { //si el menu de email esta abierto
-        //cierro el menu de email
-        desktopMenu.classList.add('inactive');
-    }
-
-    //muestro el menu de carrito
-    shopingCartContainer.classList.toggle('inactive');
-}
+//agrego evento click al btn de cerrar el detalle del producto
+btnCloseProductDetail.addEventListener('click', closeProductDetail);
 
 //creo array de productos
 const productList = [];
@@ -88,6 +52,75 @@ productList.push({
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 
+//funcion para mostrar el menu de email
+function menuEmailClick() {
+    const isAsaideClosed = shopingCartContainer.classList.contains('inactive'); //si el menu de carrito esta cerrado
+
+    if(!isAsaideClosed) { //si el menu de carrito esta abierto
+        //cierro el menu de carrito
+        shopingCartContainer.classList.add('inactive');
+    }
+
+    desktopMenu.classList.toggle('inactive'); //el nombre de la clase va sin el punto
+}
+//funcion para mostrar el menu de hamburgesa
+function menuHamburgesaClick() {
+    const isAsaideClosed = shopingCartContainer.classList.contains('inactive'); //si el menu de carrito esta cerrado
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive'); //si el detalle del producto esta cerrado
+
+    if(!isAsaideClosed) { //si el menu de carrito esta abierto
+        //cierro el menu de carrito
+        shopingCartContainer.classList.add('inactive');
+    }
+
+    if(!isProductDetailClosed) { //si el detalle del producto esta abierto
+        //cierro el detalle del producto
+        productDetailContainer.classList.add('inactive');
+    }
+
+    menuMobile.classList.toggle('inactive');
+}
+//funcion para mostrar el menu de carrito
+function iconoCarritoClick() {
+    const isMenuMobileClosed = menuMobile.classList.contains('inactive'); //si el menu de hamburgesa esta cerrado
+    const isMenuEmailClosed = desktopMenu.classList.contains('inactive'); //si el menu de email esta cerrado
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive'); //si el detalle del producto esta cerrado
+
+    if(!isMenuMobileClosed) { //si el menu de hamburgesa esta abierto
+        //cierro el menu de hamburgesa
+        menuMobile.classList.add('inactive');
+    }
+
+    if(!isMenuEmailClosed) { //si el menu de email esta abierto
+        //cierro el menu de email
+        desktopMenu.classList.add('inactive');
+    }
+
+    if(!isProductDetailClosed) { //si el detalle del producto esta abierto
+        //cierro el detalle del producto
+        productDetailContainer.classList.add('inactive');
+    }
+
+    //muestro el menu de carrito
+    shopingCartContainer.classList.toggle('inactive');
+}
+
+//funcion para abrir el detalle del producto
+function openProductDetail() {
+    //siempre q abra el detalle del producto cierro el menu de carrito
+    shopingCartContainer.classList.add('inactive'); //agrego la clase inactive
+    //siempre q abra el detalle del producto cierro el menu de hamburgesa
+    menuMobile.classList.add('inactive'); //agrego la clase inactive
+    //muestro el detalle del producto
+    productDetailContainer.classList.remove('inactive'); //remuevo la clase inactive
+}
+
+//funcion para cerrar el detalle del producto
+function closeProductDetail() {
+    //cierro el detalle del producto
+    productDetailContainer.classList.add('inactive'); //agrego la clase inactive
+}
+
 //funcion renderiza productos
 function renderProducts(arrProds) {
     //recorro el array de productos
@@ -98,12 +131,18 @@ function renderProducts(arrProds) {
         const productCard = document.createElement('div');
         //agrego la clase al elemento div
         productCard.classList.add('product-card');
+
         //creo el elemento img
         const img = document.createElement('img');
         //agrego la clase al elemento img
         img.classList.add('imgCard');
         //agrego el atributo src al elemento img
         img.setAttribute('src', product.image);
+        //agrego el atributo alt al elemento img
+        img.setAttribute('alt', product.name);
+        //agrego evento click al elemento img
+        img.addEventListener('click', openProductDetail); //openProductDetail es una funcion que todavia no cree
+
         //creo el elemento div
         const contTextYicono = document.createElement('div');
         //agrego la clase al elemento div
